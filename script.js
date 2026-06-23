c3ba15a40e6362ec4f05bad67c1329827c6bc91b
Mrzenn510 <riztotod@gmail.com>
2026-06-23 14:56:19 +0700

    Replace setInterval with jam function for clock and particles
    Refactor clock update and particle animation into a function.

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
-setInterval(()=>{
-document.getElementById("clock").innerHTML=new Date().toLocaleTimeString("id-ID");
-},1000);
+function jam(){
+let now = new Date();
 
-const c=document.getElementById("particles");
-const ctx=c.getContext("2d");
-c.width=innerWidth;c.height=innerHeight;
-let p=[...Array(80)].map(()=>({x:Math.random()*c.width,y:Math.random()*c.height,r:Math.random()*2+1}));
-function draw(){
-ctx.clearRect(0,0,c.width,c.height);
-ctx.fillStyle="#7c3aed";
-p.forEach(i=>{ctx.beginPath();ctx.arc(i.x,i.y,i.r,0,6.28);ctx.fill();i.y+=0.3;if(i.y>c.height)i.y=0;});
-requestAnimationFrame(draw);
+document.getElementById("clock").innerHTML =
+now.toLocaleTimeString("id-ID");
 }
-draw();
+
+setInterval(jam,1000);
+jam();
